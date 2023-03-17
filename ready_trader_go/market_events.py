4d78c1ca02fcb739453b392a269bf34a9d903b97
Matyas Huba <hubamatyas@gmail.com>
2023-03-17 23:43:32 +0000

    analysed number 2

--- ready_trader_go/market_events.py
+++ ready_trader_go/market_events.py
@@ -161,6 +161,7 @@ class MarketEventsReader(IOrderListener):
             csv_reader = csv.reader(market_data)
             next(csv_reader)  # Skip header row
             for row in csv_reader:
+                print(row)
                 # time, instrument, operation, order_id, side, volume, price, lifespan
                 fifo.put(MarketEvent(float(row[0]), Instrument(int(row[1])), MarketEventOperation[row[2]],
                                      int(row[3]), Side[row[4]] if row[4] else None,
